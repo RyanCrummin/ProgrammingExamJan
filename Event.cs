ce39f6c789bdd608f40b696be551299666beafff
Ryan Crumley <S00253911@atu.ie>
2025-01-08 13:18:01 +0000

    Event class made, going to start on the IComparable Interface

---
+++ Event.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JanuaryExam
+{
+    public class Event
+    {
+        string Name {  get; set; }
+        DateTime EventDate {  get; set; }
+        List<Ticket> Tickets { get; set; }
+        enum EventType
+        {
+            Music,
+            Comedy,
+            Theatre
+        }
+    }
+}
